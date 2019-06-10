0a124a1bd4d5cd0bb4e51659991817d81cd4e490
haijunli <al@shinghoo.com>
2019-06-10 16:50:11 +0800

    updated

--- commons.go
+++ commons.go
@@ -64,6 +64,59 @@ func SaveImage(imgFile multipart.File, savePath string) (savedPath string) {
 	return
 }
 
+func SaveImageFromTmpFile(imgTmpFileName string, savePath string) (savedPath string) {
+	imgFile, err := os.Open(imgTmpFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(imgFile)
+	savedFileName := GetRandomHashString()
+	buffer := &bytes.Buffer{}
+	_, err = io.Copy(buffer, imgFile)
+	if err != nil {
+		panic(err)
+	}
+	fileBytes, _ := ioutil.ReadAll(buffer)
+	headData := fileBytes[:512]
+
+	if contentType := http.DetectContentType(headData); contentType != "" {
+		switch contentType {
+		case "image/gif":
+			savedFileName += ".gif"
+			break
+		case "image/png":
+			savedFileName += ".png"
+			break
+		case "image/jpg":
+			savedFileName += ".jpg"
+			break
+		case "image/jpeg":
+			savedFileName += ".jpg"
+			break
+		}
+	}
+	if _, err = os.Stat(savePath); os.IsNotExist(err) {
+		err = os.MkdirAll(savePath, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	savedPath = fmt.Sprintf("%s/%s", savePath, savedFileName)
+	err = ioutil.WriteFile(savedPath, fileBytes, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
+
 func GetRandomHashString() (str string) {
 	u1, _ := uuid.NewV4()
 	str = u1.String()
